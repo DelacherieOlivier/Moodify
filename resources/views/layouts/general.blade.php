cd355903c2a323d3cb4e8964e51c8bc567e12e5f
DelacherieOlivier <35688948+DelacherieOlivier@users.noreply.github.com>
2020-05-20 15:54:14 +0200

    maj
    maj

--- resources/views/layouts/general.blade.php
+++ resources/views/layouts/general.blade.php
@@ -7,7 +7,7 @@
             <meta charset='UTF-8'>
             <meta name="csrf-token" content="{{ csrf_token() }}">
             <link rel="stylesheet" href="/css/style.css">
-            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">    <title>Mon application soundcloud</title>
+            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">    <title>Moodify</title>
             <link href="https://fonts.googleapis.com/css?family=Mukta&display=swap" rel="stylesheet">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
         </head>
